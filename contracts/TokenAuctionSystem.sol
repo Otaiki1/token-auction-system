084383578f0f687a8aa552c83557b82100a6505a
Otaiki1 <otaikisadiq2804@gmail.com>
2023-03-30 02:44:50 +0100

    chore: comment code properly

--- contracts/TokenAuctionSystem.sol
+++ contracts/TokenAuctionSystem.sol
@@ -1,120 +1,143 @@
 // SPDX-License-Identifier: MIT
 
+// Declare the version of Solidity being used
 pragma solidity 0.8.18;
 
+// Import the ERC721 interface from OpenZeppelin library
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+// Import ReentrancyGuard contract from OpenZeppelin for security
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract TokenAuctionSystem is ReentrancyGuard{
+// Define the main contract
+contract TokenAuctionSystem is ReentrancyGuard {
 
-    //store token schema
-    struct Auction{
-        address seller;
-
-        address currentHighestBidder;
-        uint256 currentHighestBid;
-        uint256 reservePrice;
-        uint256 startTime;
-        uint256 endTime;
- 
+    // Struct to hold auction details for a given token
+    struct Auction {
+        address seller;              // Address of the seller
+        address currentHighestBidder;// Address of the current highest bidder
+        uint256 currentHighestBid;   // The current highest bid for the token
+        uint256 reservePrice;        // The reserve price set for the token
+        uint256 startTime;           // The time when the auction started
+        uint256 endTime;             // The time when the auction will end
     }
 
-    struct Bid{
-        address bidderAddress;
-        uint256 bidAmount;
+    // Struct to hold a bid for a given token
+    struct Bid {
+        address bidderAddress;  // Address of the bidder
+        uint256 bidAmount;      // The amount of the bid
     }
 
-    struct Token{
-        uint256 tokenId;
-        address tokenOwner;
-        IERC721 tokenCA;
+    // Struct to hold the details of a given token
+    struct Token {
+        uint256 tokenId;        // ID of the token
+        address tokenOwner;     // Address of the owner of the token
+        IERC721 tokenCA;        // Interface for the token contract
     }
 
-    
-    //mapping of token id to tokens
-    mapping(uint256 => Token) tokens;
-    //tokenId to Auction structs
-    mapping(uint256  => Auction) auctions;
-    //tokenId to bids
-    mapping(uint256 => Bid) bids;
+    // Mapping to associate each token ID with its respective Token struct
+    mapping(uint => Token) tokens;
+    // Mapping to associate each token ID with its respective Auction struct
+    mapping(uint => Auction) auctions;
+    // Mapping to associate each token ID with its respective Bid struct
+    mapping(uint => Bid) bids;
 
-    modifier onlySellerOrWinner(uint256 _tokenId){
+    // Modifier to restrict access to only the seller or the current highest bidder
+    modifier onlySellerOrWinner(uint _tokenId){
         require(msg.sender == auctions[_tokenId].seller || msg.sender == auctions[_tokenId].currentHighestBidder, "NOT ALLOWED");
         _;
     }
-  
-    
-    function createAuction(uint256 _tokenId, uint256 _reservePrice, IERC721 _tokenCA) external nonReentrant{
+
+    // Function to create an auction for a token
+    function createAuction(uint _tokenId, uint _reservePrice, IERC721 _tokenCA) external nonReentrant {
+        // Check that the caller is the owner of the token
         require(_tokenCA.ownerOf(_tokenId) == msg.sender, "NOT OWNER OF TOKEN");
+        // Check that the reserve price is not zero
         require(_reservePrice != 0, "PRICE CANT BE ZERO");
 
-        //update Mappings
+        // Create a new Token struct and add it to the tokens mapping
         tokens[_tokenId] = Token(_tokenId, msg.sender, _tokenCA);
+        // Create a new Auction struct and add it to the auctions mapping
         auctions[_tokenId] = Auction(msg.sender, address(0), 0, _reservePrice, 0, 0);
 
-        //transfer token to contract
+        // Transfer the token to the contract
         _tokenCA.transferFrom(msg.sender, address(this), _tokenId);
-
-
     }
- 
-    function placeBid(uint256 _tokenId) external payable nonReentrant returns(bool isPlaced) {
-        //check to ensure the token is auctioned
+
+    // Function to place a bid on a token
+    function placeBid(uint256 _tokenId) external payable nonReentrant returns(bool isPlaced){
+        // Check that the token is being auctioned
         Auction memory auctionToken = auctions[_tokenId];
         require(auctionToken.seller != address(0), "Token not auctioned");
+        // Check that the bid amount is higher than the reserve price
         require(msg.value > auctionToken.reservePrice, "bid amount must be greater than reservePrice");
-        //check  if this is first bid 
-        if(auctionToken.currentHighestBidder == address(0)){
-            auctionToken.startTime  = block.timestamp;
-            auctionToken.endTime = block.timestamp +15 minutes;
+
+        // If this is the first bid, set the start and end
+ if (auctionToken.currentHighestBidder == address(0)) {
+            auctionToken.startTime = block.timestamp;
+            auctionToken.endTime = block.timestamp + 15 minutes;
         }
 
-        if(msg.value <= auctionToken.currentHighestBid){
-           (bool success, ) =  payable(msg.sender).call{value: msg.value}("");
-           if(success){
-            return false;
-           }else{
-            revert("return payment failed");
-           }
-        }else{
+        //if the bid amount is not higher than the current highest bid, refund the sender
+        if (msg.value <= auctionToken.currentHighestBid) {
+            (bool success, ) = payable(msg.sender).call{value: msg.value}("");
+            if (success) {
+                return false;
+            } else {
+                revert("return payment failed");
+            }
+        } else {
+            //if the bid amount is higher than the current highest bid, update the mappings
             auctionToken.currentHighestBid = msg.value;
             auctionToken.currentHighestBidder = msg.sender;
-            //update bids mapping
             bids[_tokenId] = Bid(msg.sender, msg.value);
+
             //return true on successful bid
             return true;
         }
-
-
     }
 
- 
+     /**
+     * @dev Closes an ongoing auction for the specified token ID and transfers ownership to the highest bidder.
+     *      The auction can only be closed by the seller or the current highest bidder.
+     *      The auction can only be closed after its end time has elapsed.
+     *      The auctioned token is transferred to the highest bidder and the seller receives the sold price.
+     * @param _tokenId uint256 ID of the token being auctioned.
+     */
     function closeAuction(uint256 _tokenId) external nonReentrant onlySellerOrWinner(_tokenId){
         //check to ensure the token end time has reached
         Auction memory auctionToken = auctions[_tokenId];
         require(block.timestamp > auctionToken.endTime, "Auction time hasnt elapsed");
 
+        //get the winner and owner of the token
         address winner = auctionToken.currentHighestBidder;
         address owner= auctionToken.seller;
         uint256 soldPrice = auctionToken.currentHighestBid;
+
         //update all mapping
         auctionToken = Auction(address(0), address(0), 0, 0, 0 ,0);
         bids[_tokenId] = Bid(address(0), 0);
-        //transfer token
+
+        //transfer token to winner
         Token memory token  = tokens[_tokenId];
         token.tokenOwner = winner;
         token.tokenCA.transferFrom(address(this), winner, _tokenId);
+
         //pay auctioner
         (bool success, ) = payable(owner).call{value: soldPrice}("");
         if(!success){
             revert("pay auctioneer failed");
         }
     }
-    
 
+    /**
+     * @dev Returns the current owner of the specified token.
+     * @param _tokenId uint256 ID of the token.
+     * @return address of the token owner.
+     */
     function getTokenOwner(uint256 _tokenId) external view returns(address){
         return tokens[_tokenId].tokenOwner;
     }
+
     
 
 }                                      
